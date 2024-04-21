a61f3bf946770668393aea1e9ae8fee0bcb073cf
Max Shen <m4xshen@gmail.com>
2024-04-21 19:23:46 +0800

    feat: create task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,13 @@
+import styles from "./Task.module.css";
+
+export function Task({ children, name, description }) {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.mainTask}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.description}>{description}</div>
+      </div>
+      {children ? <div className={styles.subTasks}>{children}</div> : null}
+    </div>
+  );
+}
