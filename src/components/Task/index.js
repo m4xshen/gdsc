dfaffada185025d2125b5473a1c2459d94da1f68
Max Shen <m4xshen@gmail.com>
2024-05-17 12:42:02 +0800

    refactor: refine svg usage

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState } from 'react';
 import styles from './styles.module.css';
 
 export default function Task({ initTask, initSubtasks, onClick }) {
@@ -16,24 +16,6 @@ export default function Task({ initTask, initSubtasks, onClick }) {
     setIsDeleted(true);
   };
 
-  useEffect(() => {
-    const labels = document.querySelectorAll(`.${styles.label}`);
-    labels.forEach(label => {
-      if (!label.querySelector('svg')) { // Check if the label already contains an SVG
-        const svgIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svgIcon.setAttribute('viewBox', '0 0 448 512');
-        svgIcon.setAttribute('width', '16');
-        svgIcon.setAttribute('height', '16');
-
-        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        path.setAttribute('d', 'M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z');
-        svgIcon.appendChild(path);
-
-        label.insertBefore(svgIcon, label.firstChild);
-      }
-    });
-  }, [task.labels]); // Run this effect whenever the task labels change
-
   if (isDeleted) {
     return null;
   }
@@ -45,7 +27,7 @@ export default function Task({ initTask, initSubtasks, onClick }) {
           <button
             className={styles.doneButton}
             onClick={() => {
-              subtasks.map(subtask => {
+              subtasks.map((subtask) => {
                 subtask.done = !task.done;
               });
               setTask({
@@ -54,11 +36,18 @@ export default function Task({ initTask, initSubtasks, onClick }) {
               });
             }}
           >
-            {"\u2713"}
+            {'\u2713'}
           </button>
 
           <button className={styles.deletebutton} onClick={handleDelete}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              width="16"
+              height="16"
+            >
+              <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+            </svg>
           </button>
 
           <div
@@ -77,6 +66,14 @@ export default function Task({ initTask, initSubtasks, onClick }) {
         <div className={styles.labels}>
           {task.labels?.map((label, index) => (
             <span key={index} className={styles.label}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                width="16"
+                height="16"
+              >
+                <path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+              </svg>
               {label}
             </span>
           ))}
@@ -105,8 +102,12 @@ export default function Task({ initTask, initSubtasks, onClick }) {
 
       <div className={styles.subtasksAndDetail}>
         <div className={styles.subTasks}>
-          {subtasks.map(subtask => (
-            <Task key={subtask.id} initTask={subtask} initSubtasks={subtask.subtasks} />
+          {subtasks.map((subtask) => (
+            <Task
+              key={subtask.id}
+              initTask={subtask}
+              initSubtasks={subtask.subtasks}
+            />
           ))}
         </div>
 
@@ -127,7 +128,9 @@ export default function Task({ initTask, initSubtasks, onClick }) {
           ></textarea>
           <textarea
             ref={descriptionRef}
-            className={detailBox ? styles.detailBoxDescription : styles.invisible}
+            className={
+              detailBox ? styles.detailBoxDescription : styles.invisible
+            }
             placeholder="Input Task Description."
             defaultValue={detailBox ? detailBox.task.description : null}
           ></textarea>
